c32255d664fce0b8966ff42c66b92c657ef383f1
George Miracle <97882379+georgemiraclee@users.noreply.github.com>
2024-04-14 19:23:06 +0700

    edit tipis

--- selesai.php
+++ selesai.php
@@ -12,7 +12,7 @@ $restoran = $_SESSION['restoran'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     session_destroy();
-    header("Location: index.php");
+    header("Location: index.php");  
     exit();
 }
 ?>
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Toko Roti - Struk Pembayaran</title>
+     <title>Es Coklat Tumbuh Dewasa - Struk Pembayaran</title>
      <!-- Bootstrap CSS -->
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
      <link rel="stylesheet" type="text/css" href="css/style.css">
